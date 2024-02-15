f19f7dafb2cf2ec2499e8eac13d05188c9860776
Bakshaliyev <47086514+Bakshaliyev@users.noreply.github.com>
2024-02-15 20:47:57 +0400

    Examlar tamamlandi

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,6 +1,6 @@
 ﻿//int num1 = (357%10);
 //int num2 = 702%10;
-//int num3 = 300;
+//int num3 = 300;                   // 1 exam
 //int num4 = 750;
 //Console.WriteLine(num1+num3); Console.WriteLine(num2+num4);
 
@@ -8,11 +8,54 @@
 
 //int a = 1234;
 //int num1 = 1234 / 1000;
-//int num2 = (1234 / 1000)+2;
+//int num2 = (1234 / 1000)+2;   //  2 exam
 //int num3 = 1234 % 1230;
 //int num4 = (1234 / 1000)+1;
-//tt
+
 //    Console.WriteLine(num1+num2); Console.WriteLine(num3-num4);
+
+
+//int num1 = 753;
+//int num2 = 753 % 100;      // 3 exam
+//Console.WriteLine(num2); 
+
+
+//Console.WriteLine("Meblegi manatla daxil edin ");
+//int amount = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Illik faizi daxil edin ");         // 4 exam
+//int percent = Convert.ToInt32(Console.ReadLine());
+
+//decimal totalAmount = amount + percent;
+
+//Console.WriteLine(totalAmount / 12);
+
+//Console.WriteLine("Cevirmek istediyiniz miqdari qeyd edin ");
+//int funt = Convert.ToInt32(Console.ReadLine());                    // exam 5
+//double deyerFunt = 405.9;
+//Console.WriteLine("Cevrilir :  " + (((funt * deyerFunt) * 0.001)));                                   //exam 5
+
+
+
+
+//Console.WriteLine("Otagin eni");
+//int eni = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Otagin uzunluqu");
+//int uzunluqu = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Otagin hundurluyu");
+//int hundurluk = Convert.ToInt32(Console.ReadLine());         // exam 6
+
+//int sahe = eni * hundurluk * uzunluqu;
+
+//Console.WriteLine("Otagin sahesi: " + sahe);
+
+//Console.WriteLine("Lazim olan boya : " + ((sahe - (sahe * 0.2)) * 0.5));
+
+
+
+
 
 
 
