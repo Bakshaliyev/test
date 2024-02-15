14957621158ebe084af47128e9bfa628aec8a47d
Bakshaliyev <47086514+Bakshaliyev@users.noreply.github.com>
2024-02-15 19:21:59 +0400

    hh

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿//int num1 = (357%10);
+//int num2 = 702%10;
+//int num3 = 300;
+//int num4 = 750;
+//Console.WriteLine(num1+num3); Console.WriteLine(num2+num4);
+
+
+
+//int a = 1234;
+//int num1 = 1234 / 1000;
+//int num2 = (1234 / 1000)+2;
+//int num3 = 1234 % 1230;
+//int num4 = (1234 / 1000)+1;
+
+//    Console.WriteLine(num1+num2); Console.WriteLine(num3-num4);
+
+
+
+
+
+
