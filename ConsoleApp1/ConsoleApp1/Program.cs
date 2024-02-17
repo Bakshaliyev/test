04720fe9030be310e9d6f4f410f21e69c17e9a59
Bakshaliyev <47086514+Bakshaliyev@users.noreply.github.com>
2024-02-18 01:00:05 +0400

    Cardnumber value

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,24 +1,59 @@
-﻿Console.WriteLine("Salam! Getmek istediyiniz olkeni qeyd edin");
-Console.ReadLine();
+﻿using System.Net.WebSockets;
 
-Console.WriteLine("Gedeceyiniz yerin mesafesini qeyd edin");
-int distance=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Kartin nomresini daxil edin");
+int cardnumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Odeceyiniz mebleg :    " + distance*100 + "  AZN  ");
-
-
-Console.WriteLine("Endirim kuponundan istifade ederek endirim elde ede bilersiz");
-int discount = Convert.ToInt32(Console.ReadLine());
-
-if (discount==1234) 
+switch (cardnumber)
 {
-    Console.WriteLine("Endirimli qiymet"+ distance*50);
+
+        case 0:
+        Console.WriteLine(5);
+        break;
+
+        case 1:
+        Console.WriteLine("Deyeri: "+   10);
+        break;
+
+        case 2:
+        Console.WriteLine("Deyeri: "+ 15);
+        break;
+
+        case 3: 
+        Console.WriteLine("Deyeri: " + 20); 
+        break;
+
+        case 4:
+        Console.WriteLine("Deyeri: " + 25);
+        break;
+
+        case 5:
+        Console.WriteLine("Deyeri: " + 30);
+        break;
+
+        case 6:
+        Console.WriteLine("Deyeri: " + 35);
+        break;
+
+        case 7:
+        Console.WriteLine("Deyeri: " + 40);
+        break;
+
+        case 8:
+        Console.WriteLine("Deyeri: " + 45);
+        break;
+
+        case 9:
+        Console.WriteLine("Deyeri: " + 50);
+        break;
+
+        case 10:
+        Console.WriteLine("Deyeri: " + 55);
+        break;
+
+        default :
+        Console.WriteLine("Kart nomresi 10 qederdir ");
+        break;
+       
+
+
 }
-
-else
-{
-    Console.WriteLine("Endirim kuponu kecerli deyil");
-}
-
-  
-
