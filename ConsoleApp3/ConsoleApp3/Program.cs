4d32bafb9e39e47e8560ce860443769023379d12
Bakshaliyev <47086514+Bakshaliyev@users.noreply.github.com>
2024-03-05 16:53:18 +0400

    Traffic Police system

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,63 +1,46 @@
-﻿//int num1 = (357%10);
-//int num2 = 702%10;
-//int num3 = 300;                   // 1 exam
-//int num4 = 750;
-//Console.WriteLine(num1+num3); Console.WriteLine(num2+num4);
+﻿using ConsoleApp3;
+
+var keyValuePairs = new Dictionary<string, Dictionary<string, Person>>();
+
+keyValuePairs.Add("10AZ100", new Dictionary<string, Person>
+{
+    {
+        "Ismayil" ,
+
+        new Person()
+
+        {
+            Name= "Ismayil" ,
+
+            Age = 18 }
+
+        }
+
+});
 
 
 
+var keyValuePairs1 = new Dictionary<string, Dictionary<string, Person1>>();
+
+keyValuePairs1.Add("10AZ100", new Dictionary<string, Person1> {
+    {
+        "Hesen Eliyev kucesi" ,
+
+        new Person1()
+
+        {
+            street= "Hesen Eliyev kucesi " ,
+
+             }
+
+        }
+
+});
+
+//Console.WriteLine(keyValuePairs["10AZ100"]["Ismayil"].Name + "   " + (keyValuePairs["10AZ100"]["Ismayil"].Age + "  " + keyValuePairs1["10AZ100"]["Hesen Eliyev kucesi"].street));
+
+string input = Console.ReadLine();
 
 
-//int num1 = 1432 / 1000;
-//int num2 = 1432 % 1428;
-//int num3 = 1432 % 1429;             // exam 2
-//int num4 = 1432 % 1430;
-
-//Console.WriteLine(num1 + num3); Console.WriteLine(num2-num4);
-
-
-
-
-//int num1 = 753;
-//int num2 = 753 % 100;      // 3 exam
-//Console.WriteLine(num2); 
-
-
-//Console.WriteLine("Meblegi manatla daxil edin ");
-//int amount = Convert.ToInt32(Console.ReadLine());
-
-//Console.WriteLine("Illik faizi daxil edin ");         // 4 exam
-//int percent = Convert.ToInt32(Console.ReadLine());
-
-//decimal totalAmount = amount + percent;
-
-//Console.WriteLine(totalAmount / 12);
-
-//Console.WriteLine("Cevirmek istediyiniz miqdari qeyd edin ");
-//int funt = Convert.ToInt32(Console.ReadLine());                    // exam 5
-//double deyerFunt = 405.9;
-//Console.WriteLine("Cevrilir :  " + (((funt * deyerFunt) * 0.001)));                                   //exam 5
-
-
-
-
-//Console.WriteLine("Otagin eni");
-//int eni = Convert.ToInt32(Console.ReadLine());
-
-//Console.WriteLine("Otagin uzunluqu");
-//int uzunluqu = Convert.ToInt32(Console.ReadLine());
-
-//Console.WriteLine("Otagin hundurluyu");
-//int hundurluk = Convert.ToInt32(Console.ReadLine());         // exam 6
-
-//int sahe = eni * hundurluk * uzunluqu;
-
-//Console.WriteLine("Otagin sahesi: " + sahe);
-
-//Console.WriteLine("Lazim olan boya : " + ((sahe - (sahe * 0.2)) * 0.5));
-
-
-
-
-
+Console.WriteLine(keyValuePairs["10AZ100"]["Ismayil"].Name + "   " + (keyValuePairs["10AZ100"]["Ismayil"].Age + "  " + keyValuePairs1["10AZ100"]["Hesen Eliyev kucesi"].street));
 
